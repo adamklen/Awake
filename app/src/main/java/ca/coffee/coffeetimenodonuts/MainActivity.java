59c12adc6100d26876fa0fce3ce163f7c7a17d5a
haydentsui <me@haydentsui.ca>
2015-03-28 04:10:13 -0400

    SparkCloud code

--- app/src/main/java/ca/coffee/coffeetimenodonuts/MainActivity.java
+++ app/src/main/java/ca/coffee/coffeetimenodonuts/MainActivity.java
@@ -1,19 +1,36 @@
 package ca.coffee.coffeetimenodonuts;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
+    SparkCore core = new SparkCore();
+    test pleasework = new test();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +40,22 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
+        Button functionOne = (Button) findViewById(R.id.button);
+        Button functionTwo = (Button) findViewById(R.id.button2);
+        functionOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new MyAsyncTask().execute("HIGH");
+                Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+            }
+        });
+        functionTwo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new MyAsyncTask().execute("LOW");
+                Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 
@@ -63,4 +96,42 @@ public class MainActivity extends ActionBarActivity {
             return rootView;
         }
     }
+    private class MyAsyncTask extends AsyncTask<String, Integer, Double> {
+
+        @Override
+        protected Double doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            postData(params[0]);
+            return null;
+        }
+
+        protected void onPostExecute(Double result){
+            Toast.makeText(getApplicationContext(), "command sent", Toast.LENGTH_LONG).show();
+        }
+
+        public void postData(String valueIWantToSend) {
+            SparkCoreRest data = new SparkCoreRest();
+            // Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("https://api.spark.io/v1/devices/" + data.getDeviceID()+"/digitalwrite");;
+
+            try {
+                // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("access_token", data.getAccessToken()));
+                nameValuePairs.add(new BasicNameValuePair("params", "A7,"+ valueIWantToSend));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+            }
+        }
+
+    }
 }
+
--- app/src/main/java/ca/coffee/coffeetimenodonuts/SparkCoreRest.java
+++ app/src/main/java/ca/coffee/coffeetimenodonuts/SparkCoreRest.java
@@ -4,4 +4,17 @@ package ca.coffee.coffeetimenodonuts;
  * Created by haydentsui on 15-03-28.
  */
 public class SparkCoreRest {
+
+    public final String deviceID = "51ff6f065082554958430887";
+    public final String accessToken = "2043d3ab2117e4d72d4e7df1e830b01f9cb22332";
+
+    public String getAccessToken()
+    {
+        return accessToken;
+    }
+
+    public String getDeviceID()
+    {
+        return deviceID;
+    }
 }
