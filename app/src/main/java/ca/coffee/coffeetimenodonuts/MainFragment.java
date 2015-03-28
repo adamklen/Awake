f1448aed7755569cf943eaf02544d5eec92e6bcc
ajklen <adam.j.klen@gmail.com>
2015-03-28 02:41:15 -0400

    The user can pick an arbitrary time of day and set the alarm to trigger at that time.

--- app/src/main/java/ca/coffee/coffeetimenodonuts/MainFragment.java
+++ app/src/main/java/ca/coffee/coffeetimenodonuts/MainFragment.java
@@ -3,6 +3,7 @@ package ca.coffee.coffeetimenodonuts;
 import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,16 +12,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextClock;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 
 /**
  * Created by ajklen on 3/28/15.
  */
-public class MainFragment extends Fragment implements View.OnClickListener {
+public class MainFragment extends Fragment implements View.OnClickListener, TimePickerDialog.OnTimeSetListener{
 
-    private TextClock clock;
-    private Calendar calendar;
+    private TextView clock;
+    private Calendar alarmTime;
+    private DateFormat format;
 
     public MainFragment () {
 
@@ -32,11 +37,13 @@ public class MainFragment extends Fragment implements View.OnClickListener {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         rootView.findViewById(R.id.button_set).setOnClickListener(this);
-        clock = (TextClock) rootView.findViewById(R.id.clock);
+        clock = (TextView) rootView.findViewById(R.id.clock);
         clock.setOnClickListener(this);
 
-        calendar = Calendar.getInstance();
-        calendar.add(Calendar.SECOND, 5);
+        alarmTime = (Calendar) Calendar.getInstance().clone();
+        format = android.text.format.DateFormat.getTimeFormat(getActivity().getApplicationContext());
+
+        clock.setText(format.format(alarmTime.getTime()));
 
         return rootView;
     }
@@ -56,13 +63,7 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     }
 
     private void setAlarm(){
-
-        if (calendar != null){
-
-            Log.d("MainFragment", "alarm set");
-
-            calendar = Calendar.getInstance();
-            calendar.add(Calendar.SECOND, 5);
+        if (alarmTime != null){
 
             Intent intent = new Intent(getActivity(), AlarmReceiverActivity.class);
             PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(),
@@ -70,7 +71,7 @@ public class MainFragment extends Fragment implements View.OnClickListener {
                     12345, intent, PendingIntent.FLAG_CANCEL_CURRENT);
 
             AlarmManager am = (AlarmManager)getActivity().getSystemService(Activity.ALARM_SERVICE);
-            am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+            am.set(AlarmManager.RTC_WAKEUP, alarmTime.getTimeInMillis(),
                     pendingIntent);
 
 
@@ -78,6 +79,23 @@ public class MainFragment extends Fragment implements View.OnClickListener {
     }
 
     private void setTime(){
+        new TimePickerDialog(getActivity(), this, alarmTime.get(Calendar.HOUR_OF_DAY), alarmTime.get(Calendar.MINUTE), false).show();
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+        Calendar current = Calendar.getInstance();
+        alarmTime = (Calendar)current.clone();
+
+        alarmTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        alarmTime.set(Calendar.MINUTE, minute);
+
+        if (alarmTime.before(current)){
+            alarmTime.add(Calendar.DAY_OF_YEAR, 1);
+        }
+
+        clock.setText(format.format(alarmTime.getTime()));
 
     }
 }
